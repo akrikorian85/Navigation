f1fd0e49fdb019408b08205510e3f66ac3c0c842
Alex Krikorian <akrikorian@envivent.com>
2019-01-08 16:34:11 -0800

    update: add navigiation classes

---
+++ Navigation.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace MyFramework.Navigation
+{
+  public class Navigation
+  {
+    private List<Page> Pages { get; set; }
+    private string CurrentPath { get; set; }
+
+    public Navigation(string path)
+    {
+      Pages = GetJson(path);
+      CurrentPath = HttpContext.Current.Request.Path;
+      SetActivePage(Pages);
+    }
+    private List<Page> GetJson(string path)
+    {
+      using (StreamReader reader = new StreamReader(path))
+      {
+        string json = reader.ReadToEnd();
+
+        return JsonConvert.DeserializeObject<List<Page>>(json);
+      }
+    }
+    private void SetActivePage(List<Page> pages, Page parentPage = null)
+    {
+
+      foreach (var page in pages)
+      {
+        if (page.ChildPages != null)
+        {
+          SetActivePage(page.ChildPages, page);
+        }
+
+        page.IsActive = page.Link.ToLower() == CurrentPath.ToLower();
+
+        if (page.IsActive || page.HasActiveChild)
+        {
+          if (parentPage != null)
+          {
+            parentPage.HasActiveChild = true;
+          }
+        }
+      }
+    }
+    private string BuildMenu(List<Page> pages, bool isSubmenu = false)
+    {
+      string buffer = "";
+      buffer += isSubmenu ? "<ul class='submenu'>" : "<ul class='menu'>";
+
+      foreach (var page in pages)
+      {
+        string classes = GetClasses(page);
+        buffer += "<li";
+
+        buffer += !String.IsNullOrEmpty(classes) ? " class='" + classes + "'" : "";
+        buffer += ">";
+        buffer += "<a href='" + page.Link + "'>" + page.Title + "</a>";
+
+        if (page.ChildPages != null)
+        {
+          string submenu = BuildMenu(page.ChildPages, true);
+          buffer += submenu;
+        }
+        buffer += "</li>";
+      }
+      buffer += "</ul>";
+
+      return buffer;
+    }
+    private string GetClasses(Page page)
+    {
+      List<string> classes = new List<string>();
+
+      if (page.IsActive)
+      {
+        classes.Add("active");
+      }
+      if (page.HasActiveChild)
+      {
+        classes.Add("has-active-child");
+      }
+      if (page.Class != null)
+      {
+        classes.Add(page.Class);
+      }
+      if (page.ChildPages != null)
+      {
+        classes.Add("has-children");
+      }
+
+      return String.Join(" ", classes);
+    }
+    public HtmlString Render()
+    {
+      HtmlString menu = new HtmlString(BuildMenu(Pages));
+
+      return menu;
+    }
+  }
+}
---
+++ Page.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFramework.Navigation
+{
+  public class Page
+  {
+    public string Title { get; set; }
+    public string Link { get; set; }
+    public string Target { get; set; }
+    public string Class { get; set; }
+    public bool HasActiveChild { get; set; }
+    public bool IsActive { get; set; }
+    public List<Page> ChildPages { get; set; }
+  }
+}
