d0da4494c5fb7aad3ecad9127de6920ae57c2a0c
Alex Krikorian <akrikorian@envivent.com>
2019-01-09 12:52:15 -0800

    update: added comments

--- Navigation.cs
+++ Navigation.cs
@@ -7,18 +7,35 @@ using Newtonsoft.Json;
 
 namespace MyFramework.Navigation
 {
+  /// <summary>
+  /// A HTML navigation builder that uses JSON to create an unordered list of navigation links
+  /// </summary>
   public class Navigation
   {
+    /// <summary>
+    /// List of Page objects that are created from JSON
+    /// </summary>
     private List<Page> Pages { get; set; }
+    /// <summary>
+    /// Path of the Http request
+    /// </summary>
     private string CurrentPath { get; set; }
-
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="path">String The path to the JSON file</param>
     public Navigation(string path)
     {
-      Pages = GetJson(path);
+      Pages = JsonToPages(path);
       CurrentPath = HttpContext.Current.Request.Path;
       SetActivePage(Pages);
     }
-    private List<Page> GetJson(string path)
+    /// <summary>
+    /// Converts JSON to a list of Page objects to be worked with 
+    /// </summary>
+    /// <param name="path">String The path to the JSON file</param>
+    /// <returns>List List of Page objects</returns>
+    private List<Page> JsonToPages(string path)
     {
       using (StreamReader reader = new StreamReader(path))
       {
@@ -27,6 +44,11 @@ namespace MyFramework.Navigation
         return JsonConvert.DeserializeObject<List<Page>>(json);
       }
     }
+    /// <summary>
+    /// Sets the active page by comparing each page's URL with the current requested URL. It also sets a flag for the active page's descendants for CSS styling
+    /// </summary>
+    /// <param name="pages"> A list of Page objects</param>
+    /// <param name="parentPage">The parent Page object</param>
     private void SetActivePage(List<Page> pages, Page parentPage = null)
     {
 
@@ -37,8 +59,9 @@ namespace MyFramework.Navigation
           SetActivePage(page.ChildPages, page);
         }
 
-        page.IsActive = page.Link.ToLower() == CurrentPath.ToLower();
+        page.IsActive = page.Href.ToLower() == CurrentPath.ToLower();
 
+        // assigns all the ancestors' of the active page property HasActiveChild to true to flag BuildMenu to add a CSS class
         if (page.IsActive || page.HasActiveChild)
         {
           if (parentPage != null)
@@ -48,6 +71,12 @@ namespace MyFramework.Navigation
         }
       }
     }
+    /// <summary>
+    /// Builds a the nav as a string that is an HTML unordered list
+    /// </summary>
+    /// <param name="pages">List<Page> A list of Page objects</param>
+    /// <param name="isSubmenu">Boolean A flag that tells the builder that the menu is a submenu (not at the root of the menu tree)</param>
+    /// <returns>String The nav as an unordered list</returns>
     private string BuildMenu(List<Page> pages, bool isSubmenu = false)
     {
       string buffer = "";
@@ -60,9 +89,9 @@ namespace MyFramework.Navigation
 
         buffer += !String.IsNullOrEmpty(classes) ? " class='" + classes + "'" : "";
         buffer += ">";
-        buffer += "<a href='" + page.Link + "'";
+        buffer += "<a href='" + page.Href + "'";
         buffer += page.Target != null ? " target='" + page.Target + "'" : "";
-        buffer += ">" + page.Title + "</a>";
+        buffer += ">" + page.Text + "</a>";
 
         if (page.ChildPages != null)
         {
@@ -75,6 +104,11 @@ namespace MyFramework.Navigation
 
       return buffer;
     }
+    /// <summary>
+    /// Checks all flags of the Page objects and returns a list of HTML classes
+    /// </summary>
+    /// <param name="page">Page The page data</param>
+    /// <returns>String a string of HTML classes that are seperated by a space</returns>
     private string GetClasses(Page page)
     {
       List<string> classes = new List<string>();
@@ -98,10 +132,14 @@ namespace MyFramework.Navigation
 
       return String.Join(" ", classes);
     }
+    /// <summary>
+    /// Returns an HTML encoded string of the nav
+    /// </summary>
+    /// <returns>HtmlString the nav</returns>
     public HtmlString Render()
     {
       HtmlString menu = new HtmlString(BuildMenu(Pages));
-
+      
       return menu;
     }
   }
--- Page.cs
+++ Page.cs
@@ -5,14 +5,38 @@ using System.Web;
 
 namespace MyFramework.Navigation
 {
+  /// <summary>
+  /// Represents a navigation link for the Navigation object, eventually being outputted as HTML
+  /// </summary>
   public class Page
   {
-    public string Title { get; set; }
-    public string Link { get; set; }
+    /// <summary>
+    /// The text of the anchor tag
+    /// </summary>
+    public string Text { get; set; }
+    /// <summary>
+    /// The value of the href attribute in the anchor tag
+    /// </summary>
+    public string Href { get; set; }
+    /// <summary>
+    /// The value of the target attribute in the anchor tag
+    /// </summary>
     public string Target { get; set; }
+    /// <summary>
+    /// The value of the class attribute in the anchor tag
+    /// </summary>
     public string Class { get; set; }
+    /// <summary>
+    /// Flag used for assigning whether this page has a child element that is 'active'
+    /// </summary>
     public bool HasActiveChild { get; set; }
+    /// <summary>
+    /// Flag used for assigning whether this page is the active page
+    /// </summary>
     public bool IsActive { get; set; }
+    /// <summary>
+    /// A list of child Page objects. Used with other Page objects, it creates a tree-like navigation structure
+    /// </summary>
     public List<Page> ChildPages { get; set; }
   }
 }
